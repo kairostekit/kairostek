3833809eb6693a674baeeb437ccbf1f43268d3ca
Your Name <you@example.com>
2023-06-12 23:14:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeadLayoutComponent from './components/layout/HeadLayoutComponent.vue'
+import FooterLayoutComponent from './components/layout/FooterLayoutComponent.vue';
+</script>
+
+<template>
+    <body id="body">
+
+        <HeadLayoutComponent />
+        
+        <RouterView />
+
+        <FooterLayoutComponent />
+    </body>
+</template>
+
+<style scoped></style>
