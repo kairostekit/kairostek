4c250b46c06815d3702ce987a9b875e3206876ed
Your Name <you@example.com>
2023-06-14 20:56:36 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
+import AboutView from './views/AboutView.vue';
 </script>
 
 <template>
-  
+  <AboutView></AboutView>
+
 </template>
 
 <style scoped></style>
